a00ad12412be6ad68fc753fb99c520908ac8a486
Eigil Krogh <eigilkrogh@outlook.com>
2018-01-26 01:07:31 +0100

    Version_macOS.cs source file added to git

---
+++ timeDateCalculator/timeDateCalculator.macOS/Version_macOS.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeDateCalculator.Interfaces;
+
+[assembly: Xamarin.Forms.Dependency(typeof(TimeDateCalculator.macOS.Version_macOS))]
+namespace TimeDateCalculator.macOS
+{
+    public class Version_macOS : IAppVersion
+    {
+        public string GetAppTitle()
+        {
+            return NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleName").ToString();
+        }
+
+        public string GetVersion()
+        {
+            return NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString();
+
+        }
+
+        public string GetBuild()
+        {
+            return NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion").ToString();
+
+        }
+    }
+}
