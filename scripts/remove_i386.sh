4600861e0291037c1d58fb1ec0043aad90470fc5
Eigil Krogh <eigilkrogh@outlook.com>
2018-02-06 04:03:43 +0100

    Script to remove i386

---
+++ scripts/remove_i386.sh
@@ -0,0 +1,13 @@
+/usr/bin/lipo /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app/Contents/MonoBundle/libMonoPosixHelper.dylib -remove i386 -output /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app/Contents/MonoBundle/libMonoPosixHelper.dylib
+
+# Target _CodesignNativeLibraries:
+    /usr/bin/codesign execution started with arguments: -v --force --sign EC1CB5386BE4DBBF9A2766A31366D28745E52AD7 /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app/Contents/MonoBundle/libMonoPosixHelper.dylib
+# Target _CodesignAppBundle:
+    /usr/bin/codesign execution started with arguments: -v --force --sign EC1CB5386BE4DBBF9A2766A31366D28745E52AD7 --entitlements /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/obj/x64/Release/Entitlements.xcent /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app
+# Target _CodesignVerify:
+    /usr/bin/codesign --verify -vvvv --deep bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app 
+    bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app: valid on disk
+    bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app: satisfies its Designated Requirement
+# Target _CreateInstaller:
+#    Creating installer package
+    /usr/bin/productbuild --component /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS.app /Applications --sign "3rd Party Mac Developer Installer: Eigil Krogh (4657Q2Y6NH)" /Users/eks/projects/xamarinProjs/TimeDateCalculator/timeDateCalculator/timeDateCalculator.macOS/bin/Release/timeDateCalculator.macOS.app/timeDateCalculator.macOS-1.2.pkg 
