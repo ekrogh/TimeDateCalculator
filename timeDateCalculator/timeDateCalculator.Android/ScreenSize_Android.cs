3be366a5a5e843e53fbd40cab05e6af0d8c453c4
Eigil Krogh Sorensen <eigilkrogh@gmail.com>
2018-02-13 12:08:34 +0100

    Restored android Platform and Screen size

---
+++ timeDateCalculator/timeDateCalculator.Android/ScreenSize_Android.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Hardware;
+using TimeDateCalculator.Interfaces;
+using Android.Util;
+using Android.Content.Res;
+
+[assembly: Xamarin.Forms.Dependency(typeof(TimeDateCalculator.Droid.ScreenSize_Android))]
+namespace TimeDateCalculator.Droid
+{
+    class ScreenSize_Android : IScreenSizeInterface
+    {
+        public double GetScreenWidth()
+        {
+            return 0;
+        }
+
+        public double GetScreenHeight()
+        {
+            return 0;
+        }
+    }
+}
