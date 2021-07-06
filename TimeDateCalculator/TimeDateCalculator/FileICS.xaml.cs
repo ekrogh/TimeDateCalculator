a751916c2d58314909883633444a3982611218ac
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2021-07-06 22:51:45 +0200

    Open Save ICS under way.

---
+++ TimeDateCalculator/TimeDateCalculator/FileICS.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TimeDateCalculator.MessageThings;
+using TimeDateCalculator;
+
+namespace TimeDateCalculatorDll
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FileICS : ContentPage
+	{
+		public FileICS()
+		{
+			InitializeComponent();
+		}
+
+		//private async void SaveICSButton_Clicked(object sender, EventArgs e)
+		//{
+
+		//	IcsDescriptionMessageArgs IcsDescription = new IcsDescriptionMessageArgs
+		//	{
+		//		EventName_Summary = Summary.Text,
+		//		TheDescription = Description.Text,
+		//		Location =  LocationEntry.Text
+		//	};
+
+		//	if ((Summary.Text == null) || (Summary.Text == ""))
+		//	{
+		//		IcsDescription.EventName_Summary = "TimeDateCalculator Event";
+		//	}
+
+		//	// Fire the message
+		//	MessagingCenter.Send
+		//		(
+		//			(App)Application.Current,
+		//			MessengerKeys.IcsDescriptionEntered,
+		//			IcsDescription
+		//		);
+
+		//	_ = await Navigation.PopAsync(true);
+
+		//}
+
+	}
+}
