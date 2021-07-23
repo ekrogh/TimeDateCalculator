a118d3f579d5e08855fd370e808e3935b5b4fec5
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2021-07-23 20:28:40 +0200

    scripts updated --> gitHub

--- gitAddPushToAll.sh
+++ gitAddPushToAll.sh
@@ -8,31 +8,37 @@ misoftThere="false"
 originThere="false"
 timcapsThere="false"
 
+# for rmt in $remotes;
+# do
+# 	FetchPushsInRmt=$(git remote show $rmt)
+
+# 	if [[ $FetchPushsInRmt = *"buffalo"* ]] || [[ $FetchPushsInRmt = *"BUFFALO"* ]]
+# 	then
+# 		buffaloThere="true" ;
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"github"* ]] || [[ $FetchPushsInRmt = *"gitHub"* ]]
+# 	then
+# 		githubThere="true"
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"misoft"* ]] || [[ $FetchPushsInRmt = *"miSoft"* ]] || [[ $FetchPushsInRmt = *"azure"* ]]
+# 	then
+# 		misoftThere="true"
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"origin"* ]]
+# 	then
+# 		originThere="true"
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"tim"* ]]
+# 	then
+# 		timcapsThere="true"
+# 	fi
+
+# done
+
+remotes=$(git remote show)
 for rmt in $remotes;
 do
-	FetchPushsInRmt=$(git remote show $rmt)
-
-	if [[ $FetchPushsInRmt = *"buffalo"* ]] || [[ $FetchPushsInRmt = *"BUFFALO"* ]]
-	then
-		buffaloThere="true" ;
-	fi
-	if [[ $FetchPushsInRmt = *"github"* ]] || [[ $FetchPushsInRmt = *"gitHub"* ]]
-	then
-		githubThere="true"
-	fi
-	if [[ $FetchPushsInRmt = *"misoft"* ]] || [[ $FetchPushsInRmt = *"miSoft"* ]] || [[ $FetchPushsInRmt = *"azure"* ]]
-	then
-		misoftThere="true"
-	fi
-	if [[ $FetchPushsInRmt = *"origin"* ]]
-	then
-		originThere="true"
-	fi
-	if [[ $FetchPushsInRmt = *"tim"* ]]
-	then
-		timcapsThere="true"
-	fi
-
+    git remote show $rmt
 done
 
 DIRNAME=$(pwd)
@@ -48,11 +54,11 @@ echo dir1   = $dir1
 echo dir2   = $dir2
 echo enddir = $dir1$dir2
 
-echo buffaloThere=$buffaloThere
-echo githubThere=$githubThere
-echo misoftThere=$misoftThere
-echo originThere=$originThere
-echo timcapsThere=$timcapsThere
+# echo buffaloThere=$buffaloThere
+# echo githubThere=$githubThere
+# echo misoftThere=$misoftThere
+# echo originThere=$originThere
+# echo timcapsThere=$timcapsThere
 
 read -p "GO? Yes:CR No:nCR : " reply
 echo svar: $reply
@@ -68,29 +74,31 @@ then
         git remote remove $rmt ;
 	done
 	
-	originSet="false"
-	if [[ $githubThere = "true" ]]
-	then
-		git remote add origin git@github.com:ekrogh/$dir2.git
-		originSet="true"
-		git remote set-url --add --push origin git@github.com:ekrogh/$dir2.git ;
-	fi
-	if [[ $misoftThere = "true" ]]
-	then
-		if [[ $originSet = "false" ]]
-		then
-			git remote add origin git@ssh.dev.azure.com:v3/ekrogh/$dir2/$dir2 ;
-			originSet="true"
-		fi
-	fi
-	if [[ $buffaloThere = "true" ]] || [[ $timcapsThere = "true" ]]
-	then
-		if [[ $originSet = "false" ]]
-		then
-			git remote add origin /Volumes/share/GIT_Root/$dir1$dir2 ;
-		fi
-	fi
-	git remote set-url --add --push origin git@ssh.dev.azure.com:v3/ekrogh/$dir2/$dir2 ;
+	# originSet="false"
+	# if [[ $githubThere = "true" ]]
+	# then
+	# 	git remote add origin git@github.com:ekrogh/$dir2.git
+	# 	originSet="true"
+	# 	git remote set-url --add --push origin git@github.com:ekrogh/$dir2.git ;
+	# fi
+	# if [[ $misoftThere = "true" ]]
+	# then
+	# 	if [[ $originSet = "false" ]]
+	# 	then
+	# 		git remote add origin git@ssh.dev.azure.com:v3/ekrogh/$dir2/$dir2 ;
+	# 		originSet="true"
+	# 	fi
+	# fi
+	# if [[ $buffaloThere = "true" ]] || [[ $timcapsThere = "true" ]]
+	# then
+	# 	if [[ $originSet = "false" ]]
+	# 	then
+	# 		git remote add origin /Volumes/share/GIT_Root/$dir1$dir2 ;
+	# 	fi
+	# fi
+
+	git remote add origin git@github.com:ekrogh/$dir2.git ;
+	git remote set-url --add --push origin git@github.com:ekrogh/$dir2.git ;
 	git remote set-url --add --push origin /Volumes/share/GIT_Root/$dir1$dir2 ;
 
 
--- gitWAddPushToAll.sh
+++ gitWAddPushToAll.sh
@@ -1,37 +1,44 @@
 remotes=$(git.exe remote show)
 echo remotes=$remotes
 
+
 buffaloThere="false"
 githubThere="false"
 misoftThere="false"
 originThere="false"
 timcapsThere="false"
 
+# for rmt in $remotes;
+# do
+# 	FetchPushsInRmt=$(git.exe remote show $rmt)
+
+# 	if [[ $FetchPushsInRmt = *"buffalo"* ]] || [[ $FetchPushsInRmt = *"BUFFALO"* ]]
+# 	then
+# 		buffaloThere="true" ;
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"github"* ]] || [[ $FetchPushsInRmt = *"gitHub"* ]]
+# 	then
+# 		githubThere="true"
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"misoft"* ]] || [[ $FetchPushsInRmt = *"miSoft"* ]] || [[ $FetchPushsInRmt = *"azure"* ]]
+# 	then
+# 		misoftThere="true"
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"origin"* ]]
+# 	then
+# 		originThere="true"
+# 	fi
+# 	if [[ $FetchPushsInRmt = *"tim"* ]]
+# 	then
+# 		timcapsThere="true"
+# 	fi
+
+# done
+
+remotes=$(git.exe remote show)
 for rmt in $remotes;
 do
-	FetchPushsInRmt=$(git.exe remote show $rmt)
-
-	if [[ $FetchPushsInRmt = *"buffalo"* ]] || [[ $FetchPushsInRmt = *"BUFFALO"* ]]
-	then
-		buffaloThere="true" ;
-	fi
-	if [[ $FetchPushsInRmt = *"github"* ]] || [[ $FetchPushsInRmt = *"gitHub"* ]]
-	then
-		githubThere="true"
-	fi
-	if [[ $FetchPushsInRmt = *"misoft"* ]] || [[ $FetchPushsInRmt = *"miSoft"* ]] || [[ $FetchPushsInRmt = *"azure"* ]]
-	then
-		misoftThere="true"
-	fi
-	if [[ $FetchPushsInRmt = *"origin"* ]]
-	then
-		originThere="true"
-	fi
-	if [[ $FetchPushsInRmt = *"tim"* ]]
-	then
-		timcapsThere="true"
-	fi
-
+    git.exe remote show $rmt
 done
 
 DIRNAME=$(pwd)
@@ -47,11 +54,11 @@ echo dir1   = $dir1
 echo dir2   = $dir2
 echo enddir = $dir1$dir2
 
-echo buffaloThere=$buffaloThere
-echo githubThere=$githubThere
-echo misoftThere=$misoftThere
-echo originThere=$originThere
-echo timcapsThere=$timcapsThere
+# echo buffaloThere=$buffaloThere
+# echo githubThere=$githubThere
+# echo misoftThere=$misoftThere
+# echo originThere=$originThere
+# echo timcapsThere=$timcapsThere
 
 read -p "GO? Yes:CR No:nCR : " reply
 echo svar: $reply
@@ -67,30 +74,32 @@ then
         git.exe remote remove $rmt ;
 	done
 	
-	originSet="false"
-	if [[ $githubThere = "true" ]]
-	then
-		git.exe remote add origin git@github.com:ekrogh/$dir2.git
-		originSet="true"
-		git.exe remote set-url --add --push origin git@github.com:ekrogh/$dir2.git ;
-	fi
-	if [[ $misoftThere = "true" ]]
-	then
-		if [[ $originSet = "false" ]]
-		then
-			git.exe remote add origin git@ssh.dev.azure.com:v3/ekrogh/$dir2/$dir2 ;
-			originSet="true"
-		fi
-	fi
-	if [[ $buffaloThere = "true" ]] || [[ $timcapsThere = "true" ]]
-	then
-		if [[ $originSet = "false" ]]
-		then
-			git.exe remote add origin //BUFFALO/share/GIT_Root/$dir1$dir2 ;
-		fi
-	fi
-	git.exe remote set-url --add --push origin git@ssh.dev.azure.com:v3/ekrogh/$dir2/$dir2 ;
-	git.exe remote set-url --add --push origin //BUFFALO/share/git_Root/$dir1$dir2 ;
+	# originSet="false"
+	# if [[ $githubThere = "true" ]]
+	# then
+	# 	git.exe remote add origin git@github.com:ekrogh/$dir2.git
+	# 	originSet="true"
+	# 	git.exe remote set-url --add --push origin git@github.com:ekrogh/$dir2.git ;
+	# fi
+	# if [[ $misoftThere = "true" ]]
+	# then
+	# 	if [[ $originSet = "false" ]]
+	# 	then
+	# 		git.exe remote add origin git@ssh.dev.azure.com:v3/ekrogh/$dir2/$dir2 ;
+	# 		originSet="true"
+	# 	fi
+	# fi
+	# if [[ $buffaloThere = "true" ]] || [[ $timcapsThere = "true" ]]
+	# then
+	# 	if [[ $originSet = "false" ]]
+	# 	then
+	# 		git.exe remote add origin /Volumes/share/GIT_Root/$dir1$dir2 ;
+	# 	fi
+	# fi
+
+	git.exe remote add origin git@github.com:ekrogh/$dir2.git ;
+	git.exe remote set-url --add --push origin git@github.com:ekrogh/$dir2.git ;
+ 	git.exe remote set-url --add --push origin //BUFFALO/share/git_Root/$dir1$dir2 ;
 
 
     remotes=$(git.exe remote show)
