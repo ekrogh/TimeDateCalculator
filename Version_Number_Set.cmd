9ee272f0d9c2c563ef1b6bd9ba87eace9c446aa4
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-10 13:29:03 +0200

    Version_Number_Set.cmd cor. to be used here.

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,4 +1,3 @@
-..\projucer\Projucer.exe .\GuitarFineTune.jucer
+D:\Program_Files\Microsoft\VisualStudio\2022\Community\Common7\IDE\devenv.exe .\GTK_Experiments_TimeDateCalculator.sln
 call code .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
-D:\Program_Files\Microsoft\VisualStudio\2022\Community\Common7\IDE\devenv.exe .\Builds\VisualStudio2022\GuitarFineTune.sln
 D:\Program_Files\SmartGit\bin\smartgit.exe .
