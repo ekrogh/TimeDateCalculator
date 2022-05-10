106b5def3d500d8b2fd52210032730e47e076017
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-10 13:48:49 +0200

    Allow for debug msgs.

--- runTDC.sh
+++ runTDC.sh
@@ -1,2 +1,6 @@
-echo runTDC.sh
-$SNAP/usr/bin/mono $SNAP/TimeDateCalculator.gtk.exe > /dev/null 2>&1
+if [ $# -eq 0 ]
+then
+    $SNAP/usr/bin/mono $SNAP/TimeDateCalculator.gtk.exe > /dev/null 2>&1
+else
+    $SNAP/usr/bin/mono $SNAP/TimeDateCalculator.gtk.exe
+fi
