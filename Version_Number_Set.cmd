b7854e6f8c91793c7faad69816fc50967d0f0048
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-10 13:25:28 +0200

    Version_Number_Set.cmd added

---
+++ Version_Number_Set.cmd
@@ -0,0 +1,4 @@
+..\projucer\Projucer.exe .\GuitarFineTune.jucer
+call code .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
+D:\Program_Files\Microsoft\VisualStudio\2022\Community\Common7\IDE\devenv.exe .\Builds\VisualStudio2022\GuitarFineTune.sln
+D:\Program_Files\SmartGit\bin\smartgit.exe .
