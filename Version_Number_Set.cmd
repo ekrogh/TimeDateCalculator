6a4676b5e0a457cca1bb688d963e2bbc3b1924c9
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-10 13:37:42 +0200

    update

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,3 +1,3 @@
 D:\Program_Files\Microsoft\VisualStudio\2022\Community\Common7\IDE\devenv.exe .\GTK_Experiments_TimeDateCalculator.sln
-call code .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
+call code .\snap\snapcraft.yaml .\snap\gui\timedatecalculator.desktop
 D:\Program_Files\SmartGit\bin\smartgit.exe .
