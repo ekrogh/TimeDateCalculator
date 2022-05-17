6a573f846a8436f4219faea62ba6f12890d8667b
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-17 11:46:18 +0200

    Ver.: 6.10.33.2

--- TimeDateCalculator/TimeDateCalculator.Android/Properties/AssemblyInfo.cs
+++ TimeDateCalculator/TimeDateCalculator.Android/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
@@ -26,7 +26,7 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("6.10.33.2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
