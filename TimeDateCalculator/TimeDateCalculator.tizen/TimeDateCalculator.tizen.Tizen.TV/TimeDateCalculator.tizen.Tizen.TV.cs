7b5147994b38108f1a26fa41aa9645b9af0f3136
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-22 21:43:02 +0200

    tizen

---
+++ TimeDateCalculator/TimeDateCalculator.tizen/TimeDateCalculator.tizen.Tizen.TV/TimeDateCalculator.tizen.Tizen.TV.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace TimeDateCalculator.tizen.Tizen.TV
+{
+	internal class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+	{
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+			LoadApplication(new App());
+		}
+
+		static void Main(string[] args)
+		{
+			var app = new Program();
+			//global::Xamarin.Forms.Platform.Tizen.In
+			Forms.Init(app);
+			//global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+			app.Run(args);
+		}
+	}
+}
