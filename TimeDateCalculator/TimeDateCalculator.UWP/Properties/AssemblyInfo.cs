8dca6db35e94c19df398deabaa62e6a42da314ca
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-05-29 16:58:19 +0200

    Ver.: 6.12.35.1

--- TimeDateCalculator/TimeDateCalculator.UWP/Properties/AssemblyInfo.cs
+++ TimeDateCalculator/TimeDateCalculator.UWP/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.11.34.1")]
+[assembly: AssemblyVersion("6.12.35.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
