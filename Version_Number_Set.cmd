c53563d26148b18bf44b79f572fe43d80dd8740f
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-10-07 17:21:52 +0200

    code

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,3 +1,3 @@
 D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\TimeDateCalculator.sln
-call "D:\Program_Files\Microsoft_VS_Code_Insiders\Code - Insiders.exe" .\snap\snapcraft.yaml .\snap\gui\timedatecalculator.desktop
+call Code.exe .\snap\snapcraft.yaml .\snap\gui\timedatecalculator.desktop
 D:\Program_Files\SmartGit\bin\smartgit.exe .
